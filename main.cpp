c8ae1e7744a4b536225916afbfd15271ec304f34
Shengwen Cheng <shengwen1997.tw@gmail.com>
2019-07-04 22:14:52 +0800

    Keep arranging the code architecture

--- main.cpp
+++ main.cpp
@@ -1,122 +1,109 @@
 #include <stdio.h>
 #include <opencv2/opencv.hpp>
 
+#define IMAGE_WIDTH  640
+#define IMAGE_HEIGHT 480
+
+#define BOARD_WIDTH  6
+#define BOARD_HEIGHT 8
+
+#define IMG_SAMPLE_SIZE 15
+
 using namespace std;
 using namespace cv;
 
-#define IMAGE_WIDTH  1920
-#define IMAGE_HEIGHT 1080
-
-#define CHECKERBOARD_WIDTH  6
-#define CHECKERBOARD_HEIGHT 8
-
-#define CALIB_IMG_SAMPLE_SIZE 15
-
-using namespace std;
+/* parameters of the camera calibrator */
+vector<string> image_names;
+//board size and image_size
+Size board_size = Size(BOARD_WIDTH, BOARD_HEIGHT);
+Size image_size = Size(IMAGE_WIDTH, IMAGE_HEIGHT);
+vector<vector<Point2f>> image_2d_points;
+vector<vector<Point3f>> object_3d_points;
+//intrinsic parameters
+Mat camera_matrix, dist_coeffs;
+vector<Mat> rvecs, tvecs;
 
 int image_count = 1;
 bool checkerboard_detected = false;
 
 cv::Mat raw_image;
 
-        vector<string> m_filenames;
-        Size m_borderSize;
-        vector<vector<Point2f>> m_srcPoints;
-        vector<vector<Point3f>> m_dstPoints;
-        Size imageSize;
-        Mat cameraMatrix, distCoeffs;
-        vector<Mat> rvecs, tvecs;
-
-void setFilename()
+void set_img_filenames(void)
 {
-	m_filenames.clear();
-	m_filenames.push_back("/tmp/intrinsic_1920x1080_1.jpg"); 
-	m_filenames.push_back("/tmp/intrinsic_1920x1080_2.jpg");
-	m_filenames.push_back("/tmp/intrinsic_1920x1080_3.jpg");
-	m_filenames.push_back("/tmp/intrinsic_1920x1080_4.jpg");
-	m_filenames.push_back("/tmp/intrinsic_1920x1080_5.jpg");
-	m_filenames.push_back("/tmp/intrinsic_1920x1080_6.jpg");
-	m_filenames.push_back("/tmp/intrinsic_1920x1080_7.jpg");
-	m_filenames.push_back("/tmp/intrinsic_1920x1080_8.jpg");
-	m_filenames.push_back("/tmp/intrinsic_1920x1080_9.jpg");
-	m_filenames.push_back("/tmp/intrinsic_1920x1080_10.jpg");
-	m_filenames.push_back("/tmp/intrinsic_1920x1080_12.jpg");
-	m_filenames.push_back("/tmp/intrinsic_1920x1080_12.jpg");
-	m_filenames.push_back("/tmp/intrinsic_1920x1080_13.jpg");
-	m_filenames.push_back("/tmp/intrinsic_1920x1080_14.jpg");
-	m_filenames.push_back("/tmp/intrinsic_1920x1080_15.jpg");
+	image_names.clear();
+
+	char img_path[100] = "";
+
+	for(int i = 1; i <= IMG_SAMPLE_SIZE; i++) {
+		sprintf(img_path, "/tmp/intrinsic_%dx%d_%d.jpg", IMAGE_WIDTH,
+		        IMAGE_HEIGHT, i);
+		image_names.push_back(img_path); 
+	}
 } 
 
-void setBorderSize(const Size &borderSize){ 
-    m_borderSize = borderSize; 
-} 
-
-void addPoints(const vector<Point2f> &srcCorners,
-				 const vector<Point3f> &dstCorners)
+void add_board_points(vector<Point2f> &_2d_corners, vector<Point3f> &_3d_corners)
 {
-	m_srcPoints.push_back(srcCorners);
-	m_dstPoints.push_back(dstCorners);
+	image_2d_points.push_back(_2d_corners);
+	object_3d_points.push_back(_3d_corners);
 } 
 
-void addChessboardPoints()
-{ 
-	vector<Point2f> srcCandidateCorners; 
-	vector<Point3f> dstCandidateCorners; 
-	for(int i=0; i<m_borderSize.height; i++){ 
-		for(int j=0; j<m_borderSize.width; j++){ 
-			dstCandidateCorners.push_back(Point3f(i, j, 0.0f)); 
-		} 
-	} 
-
-	for(int i=0; i<m_filenames.size(); i++){ 
-		Mat image=imread(m_filenames[i],CV_LOAD_IMAGE_GRAYSCALE); 
-
-		imageSize = image.size();
-
-		findChessboardCorners(image, m_borderSize, srcCandidateCorners); 
-		TermCriteria param(TermCriteria::MAX_ITER + TermCriteria::EPS, 30, 0.1);
-		cornerSubPix(image, srcCandidateCorners, Size(5,5), Size(-1,-1), param);  
-		if(srcCandidateCorners.size() == m_borderSize.area()){ 
-			addPoints(srcCandidateCorners, dstCandidateCorners); 
-		} 
-	} 
-} 
-
-bool visualize_checkerboard(Mat &raw_image, Mat &chessboard_visualized_image, Size border_size)
+bool visualize_checkerboard(Mat &raw_image, Mat &board_visualized_img)
 {
 	Mat gray_image;
 	cv::cvtColor(raw_image, gray_image, cv::COLOR_BGR2GRAY);
 
 	vector<Point2f> corners;	
 
-	bool find = findChessboardCorners(gray_image, border_size, corners);
+	bool found = findChessboardCorners(gray_image, board_size, corners);
 
-	if(find == true) {
-		raw_image.copyTo(chessboard_visualized_image);
+	if(found == true) {
+		raw_image.copyTo(board_visualized_img);
 
 		TermCriteria param(TermCriteria::MAX_ITER + TermCriteria::EPS, 30, 0.1);
 		cornerSubPix(gray_image, corners, Size(5,5), Size(-1,-1), param);
-		cv::drawChessboardCorners(chessboard_visualized_image, border_size, corners, find);
+		cv::drawChessboardCorners(board_visualized_img, board_size, corners, found);
 		return true;
 	} else {
 		return false;
 	}
 }
 
-void calibrate()
+void estimate_intrinsic_parameters()
 {
-	calibrateCamera(m_dstPoints, m_srcPoints, imageSize, cameraMatrix,
-			distCoeffs, rvecs, tvecs);  
+	set_img_filenames();
+
+	vector<Point2f> _2d_corners;
+	vector<Point3f> _3d_corners;
+
+	for(int i = 0; i < board_size.height; i++) {
+		for(int j = 0; j < board_size.width; j++){
+			_3d_corners.push_back(Point3f(i, j, 0.0f));
+		}
+	}
+
+	for(int i = 0; i < image_names.size(); i++) {
+		Mat image = imread(image_names[i], CV_LOAD_IMAGE_GRAYSCALE); 
+
+		findChessboardCorners(image, board_size, _2d_corners); 
+		TermCriteria param(TermCriteria::MAX_ITER + TermCriteria::EPS, 30, 0.1);
+		cornerSubPix(image, _2d_corners, Size(5, 5), Size(-1, -1), param);  
+		if(_2d_corners.size() == board_size.area()) { 
+			add_board_points(_2d_corners, _3d_corners); 
+		} 
+	} 
+
+	calibrateCamera(object_3d_points, image_2d_points, image_size,
+			camera_matrix, dist_coeffs, rvecs, tvecs);  
 }
 
 void undistort_image(const Mat &src, Mat &dst)
 {
 	Mat map1, map2; 
 
-	calibrateCamera(m_dstPoints, m_srcPoints, imageSize, cameraMatrix,
-			distCoeffs, rvecs, tvecs);  
-	initUndistortRectifyMap(cameraMatrix, distCoeffs, Mat(), Mat(),
-				imageSize, CV_32F, map1, map2); 
+	calibrateCamera(object_3d_points, image_2d_points, image_size, camera_matrix,
+			dist_coeffs, rvecs, tvecs);  
+	initUndistortRectifyMap(camera_matrix, dist_coeffs, Mat(), Mat(),
+				image_size, CV_32F, map1, map2); 
 	remap(src, dst, map1, map2, INTER_LINEAR); 
 }
 
@@ -124,7 +111,7 @@ void on_click_callback(int event, int x, int y, int flags, void* param)
 {
 	char img_save_path[100] = "";
 
-	if((event == CV_EVENT_LBUTTONDOWN) && (image_count <= CALIB_IMG_SAMPLE_SIZE)) {
+	if((event == CV_EVENT_LBUTTONDOWN) && (image_count <= IMG_SAMPLE_SIZE)) {
 		if(checkerboard_detected == true) {
 			sprintf(img_save_path, "/tmp/intrinsic_%dx%d_%d.jpg", IMAGE_WIDTH,
 			        IMAGE_HEIGHT, image_count);
@@ -138,9 +125,10 @@ void on_click_callback(int event, int x, int y, int flags, void* param)
 }
 
 int main()
-{ 
+{
 	cv::VideoCapture camera(0);
-	Mat undistorted_image;
+	camera.set(CV_CAP_PROP_FRAME_WIDTH, IMAGE_WIDTH);
+	camera.set(CV_CAP_PROP_FRAME_HEIGHT, IMAGE_HEIGHT);
 
 	if(camera.isOpened() == false) {
 		return 0;
@@ -151,15 +139,16 @@ int main()
 
 	cout << "please click the window to save 15 images for calibration.\n";
 
+	Mat undistorted_image;
 	vector<Point2f> corners;
 
 	while(image_count <= 15) {
 		camera >> raw_image;
 
-		Mat chessboard_visualized_image;
+		Mat board_visualized_img;
 
-		if(visualize_checkerboard(raw_image, chessboard_visualized_image, Size(CHECKERBOARD_WIDTH, CHECKERBOARD_HEIGHT)) == true) {
-			cv::imshow("intrinsic calibration", chessboard_visualized_image);
+		if(visualize_checkerboard(raw_image, board_visualized_img) == true) {
+			cv::imshow("intrinsic calibration", board_visualized_img);
 			checkerboard_detected = true;
 		} else {
 			checkerboard_detected = false;
@@ -170,11 +159,7 @@ int main()
 	}
 
 	cout << "start estimating extrinsicing parameters...\n";
-
-	setFilename(); 
-	setBorderSize(Size(CHECKERBOARD_WIDTH,CHECKERBOARD_HEIGHT));
-	addChessboardPoints();
-	calibrate();
+	estimate_intrinsic_parameters();
 
 	while(1) {
 		camera >> raw_image;
@@ -185,6 +170,5 @@ int main()
 	}
 
 	destroyAllWindows();
-
 	return 0;
 }
